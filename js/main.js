87ca74516edbd4d8276c9b21a44e6250d3bb4dfa
Ram <75058788+Ramtn695@users.noreply.github.com>
2020-12-17 11:51:03 +0530

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,1 +1,6 @@
-
+document.getElementById('login').addEventListener('click',()=>{
+    document.getElementById('show').style.display='block';
+});
+document.getElementById('cancel').addEventListener('click',()=>{
+    document.getElementById('show').style.display='none';
+})
